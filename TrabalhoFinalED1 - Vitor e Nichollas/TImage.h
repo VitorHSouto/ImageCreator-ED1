7210405c3b4a26e3eb5d55178b9595d68b7f9af0
Vitor Souto <Vitor2304@github.com>
2021-06-18 15:55:48 -0300

    Teste finalizado -Criação da pasta Teste

--- TrabalhoFinalED1 - Vitor e Nichollas/TImage.h
+++
@@ -1,9 +0,0 @@
-#define SUCCESS 0
-#define INVALID_NULL_POINTER -1
-#define INVALID_FILE -5
-#include "TMat2D.h"
-
-int open_file(FILE **fp, char* arg);
-int show_image(FILE **fp);
-TMat2D* image_create(FILE **fp);
-int convert_file(char* texto, char* binario);
---
+++ TrabalhoFinalED1 - Vitor e Nichollas/Testes/teste.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "TImage.h"
+
+void ErrorManager(int error, char mensage[50]);
+
+int main(int argc, char *argv[])
+{
+	for (int i = 0; i < argc; i++)
+	{
+		printf("\nParametro %d: %s\n", i, argv[i]);
+	}
+
+	if(strcmp(argv[1], "-open") == 0)
+	{
+		ErrorManager(
+			open_file(argv[2]), "\nArquivo iniciado!\n");
+	}
+	else if(strcmp(argv[1], "-convert") == 0)
+	{
+		ErrorManager(
+			convert_file(argv[2],argv[3]), "Arquivo Convertido!\n");
+	}	
+}
+
+void ErrorManager(int error, char mensage[50])
+{
+	switch (error)
+	{
+	case SUCCESS:
+		printf("%s", mensage);
+		break;
+
+	case INVALID_NULL_POINTER:
+		printf("\n ----- FALHA NO PONTEIRO! ----- \n");
+		break;
+
+	case INVALID_FILE:
+		printf("\n ----- FALHA NO INICIO DO ARQUIVO! ----- \n");
+		break;
+
+	default:
+		break;
+	}
+}
